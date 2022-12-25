4198ab09872fc2076eff4c889654cb23c8b3898b
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2022-12-25 21:49:38 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-instagram_2.0',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
