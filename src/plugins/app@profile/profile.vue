8e2dc0789f2c8045b14b46fcf4861ecb2de5916a
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2022-12-30 23:19:08 +0100

    ion segment

--- src/plugins/app@profile/profile.vue
+++ src/plugins/app@profile/profile.vue
@@ -20,31 +20,56 @@
           :followers="user.followers"
           :following="user.following"
       />
+      <ion-segment value="default">
+        <ion-segment-button value="default">
+          <ion-icon :src="imageOutline"></ion-icon>
+        </ion-segment-button>
+        <ion-segment-button value="segment">
+          <ion-icon :src="pricetagOutline"></ion-icon>
+        </ion-segment-button>
+      </ion-segment>
     </ion-content>
     <a-footer/>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonToolbar, IonRow, IonCol, IonTitle, IonIcon, IonContent } from "@ionic/vue";
-import { add, menu } from 'ionicons/icons'
+import { IonPage, IonToolbar, IonRow, IonCol, IonTitle, IonIcon, IonContent, IonSegment, IonSegmentButton } from "@ionic/vue";
+import { add, menu, imageOutline, pricetagOutline } from 'ionicons/icons'
 import AFooter from "@/plugins/app/_layout/a-footer.vue";
 import { LoginUser } from "@/app_data/login-user";
 import AProfileUser from "@/plugins/app@profile/_components/z-user-content.vue";
 
 export default {
   name: "Profile",
-  components: {AProfileUser, AFooter, IonPage, IonToolbar, IonRow, IonCol, IonTitle, IonIcon, IonContent },
+  components: { AProfileUser, AFooter, IonPage, IonToolbar, IonRow, IonCol, IonTitle, IonIcon, IonContent, IonSegment, IonSegmentButton },
   data () {
     return {
       user: LoginUser,
       add,
-      menu
+      menu,
+      imageOutline,
+      pricetagOutline
     }
   },
 }
 </script>
 
-<style scoped>
-
+<style lang="sass">
+ion-segment-button::part(indicator-background)
+  background: #ee5b5b
+/* Material Design styles */
+ion-segment-button.md::part(native)
+  color: #000
+.segment-button-checked.md::part(native)
+  color: #ee5b5b
+ion-segment-button.md::part(indicator-background)
+  height: 4px
+/* iOS styles */
+ion-segment-button.ios::part(native)
+  color: #08a391
+.segment-button-checked.ios::part(native)
+  color: #ee5b5b
+ion-segment-button.ios::part(indicator-background)
+  border-radius: 20px
 </style>
